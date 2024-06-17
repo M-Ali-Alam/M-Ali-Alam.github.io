bcbf9878f08555dae952d3a94712628e5dda87c4
Muhammad Ali Alam <m.ali.14964@khi.iba.edu.pk>
2024-06-17 10:07:09 +0200

    Deployment issues (#13)
    * updated package.json
    
    * fixed react-router added a package
    
    package name gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from "./components";
 
 const App = () => {
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-centre">
           <Navbar />
           <Hero />
         </div>
         <div className="mt-20">
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
       </div>
-    </BrowserRouter>
-  )
-}
+    </HashRouter>
+  );
+};
 
-export default App
+export default App;
